b4c3124c70b9a542356b8f2cdfc9b8d6eb7c5f2c
Anastasia Pelevina <pelevina.2000@mail.ru>
2019-05-07 21:00:55 +0500

    Creature update

--- Jija/Jija/Creature.cs
+++ Jija/Jija/Creature.cs
@@ -1,7 +1,23 @@
+using System.Drawing;
+
 namespace Jija
 {
     public class Creature
     {
-        
+        public int Health { get; private set; }
+        public Point Position { get; private set; }
+        public int Direction;
+
+        private Point veloctiy;
+
+        public void MoveInDirection()
+        {
+            
+        }
+
+        public void Jump()
+        {
+            
+        }
     }
 }
