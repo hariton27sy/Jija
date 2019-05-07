845ccb0347874e89eff8fa6bc83a53b07a68ac14
hariton27sy <hariton27sy@gmail.com>
2019-05-08 01:39:30 +0500

    Editing Game.cs

--- Jija/Jija/Game.cs
+++ Jija/Jija/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,30 @@ namespace Jija
 {
     class Game
     {
+        public Player Player;
+        public Game(string filePath)
+        {
+            Load_Map(filePath);
+        }
+
+        private void Load_Map(string filename)
+        {
+            var y = 0;
+            foreach (var line in File.ReadLines(filename))
+            {
+                var x = 0;
+                foreach (var symbol in line)
+                {
+                    if (symbol == 'p')
+                    {
+                        Player = new Player();
+                    }
+                    
+                    x++;
+                }
+                
+                y++;
+            }
+        }
     }
 }
