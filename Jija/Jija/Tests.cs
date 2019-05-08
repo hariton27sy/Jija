53b265cb9555ad49ae55a9aa504c76f97faeccfa
Anastasia Pelevina <pelevina.2000@mail.ru>
2019-05-08 11:01:33 +0500

    Update Tests.cs

--- Jija/Jija/Tests.cs
+++ Jija/Jija/Tests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Drawing;
 using NUnit.Framework;
 
@@ -14,6 +15,16 @@ namespace Jija
             var wall = new Wall(new Point(50, 20));
             player.RepairCollision(wall);
             Assert.AreEqual(new Point(18, 20), player.Position);
+        }
+
+        [Test]
+        public void Test_PlayerEnemyCollision()
+        {
+            var player = new Player(new Point(20, 20), 3);
+            var sponge = new Sponge(new Point(20, 20));
+            GameObject.ObjectsOnMap = new List<GameObject> { player, sponge };
+            player.Update();
+            Assert.AreEqual(2, player.Health);
         }
     }
 }
