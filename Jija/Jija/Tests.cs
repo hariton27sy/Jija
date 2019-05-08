da42f38cf03f640303ead5d4e175c6c305c95d1f
hariton27sy <hariton27sy@gmail.com>
2019-05-08 07:58:00 +0500

    Tests

--- Jija/Jija/Tests.cs
+++ Jija/Jija/Tests.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using NUnit.Framework;
 
 
@@ -6,6 +7,13 @@ namespace Jija
     [TestFixture]
     public class Tests
     {
-        
+        [Test]
+        public void Test_RepairCollision()
+        {
+            var player = new Player(new Point(20, 20), 3);
+            var wall = new Wall(new Point(50, 20));
+            player.RepairWallCollision(wall);
+            Assert.AreEqual(new Point(18, 20), player.Position);
+        }
     }
 }
